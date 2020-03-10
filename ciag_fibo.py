d35d8193e591785f1f5091b03cb69efc8d5d94c9
Michal Karas <michal.m.karas@gmail.com>
2020-03-10 11:48:44 +0100

    to co użytkownik wprowadzi będzie interpretowane jako int, a nie string

--- ciag_fibo.py
+++ ciag_fibo.py
@@ -1,14 +1,13 @@
-print("podaj liczbę dla której obliczymy kod fibonacciego")
-x=input()
+x = int(input("podaj liczbę dla której obliczymy kod fibonacciego:  "))
 x0=0
 x1=1
 if x == 0:
     print(0)
 elif x == 1:
     print(1)
-else: 
-    for x in range(2,20):
+else:
+    for element in range(2,x):
+        x2=x1+x0
         x0=x1
         x1=x2
-        x2=x1+x0
-print(x2)
+    print(x2)
