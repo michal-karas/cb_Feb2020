eaad77b96a08457396c981290cf245661ad06394
Michal Karas <michal.m.karas@gmail.com>
2020-03-12 13:38:50 +0100

    zadanie domowe-palindrom

--- palindrom.py
+++ palindrom.py
@@ -10,8 +10,22 @@
 #range(90, 100) #[90, 100)
 #range(90, 100, 2) #[90, 92, 94, 96, 98]
 
-slowo=input(("Wprowadz słowo: "))
-if(slowo==slowo[::-1]):
-      print("Słowo jest palindromem")
-else:
-      print("Słowo nie jest palindromem")
+#slowo=input(("Wprowadz słowo: "))
+#if(slowo==slowo[::-1]):
+      #print("Słowo jest palindromem")
+#else:
+      #print("Słowo nie jest palindromem")
+
+
+
+str = input("Wprowadź słowo: ")
+l = len(str)
+p = l-1
+i = 0
+while i < p:
+    if str[i] != str[p-1]:
+        print("Słowo jest palindromem")
+        break
+    else:
+        print("Słowo nie jest palindromem")
+        break
