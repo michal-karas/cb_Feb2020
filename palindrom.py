5f2decfd08240fcf4ae3d5f0ee7a902c1918026d
Michal Karas <michal.m.karas@gmail.com>
2020-03-12 11:58:14 +0100

    zadanie domowe-palindrom

---
+++ palindrom.py
@@ -0,0 +1,17 @@
+#Palindrom
+#Kajak
+#Elf układał kufle
+#A to kanapa pana Kota
+
+#slowo = input("Wpisz slowo: ")
+
+# 3 formy range-a":
+#range(100) #[0, 100) domknięcie miękkie czyli nie osiaga wartości 100
+#range(90, 100) #[90, 100)
+#range(90, 100, 2) #[90, 92, 94, 96, 98]
+
+slowo=input(("Wprowadz słowo: "))
+if(slowo==slowo[::-1]):
+      print("Słowo jest palindromem")
+else:
+      print("Słowo nie jest palindromem")
