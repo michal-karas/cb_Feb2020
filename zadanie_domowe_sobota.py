2fa22e908baa5245b73649832352f7ab9eb56621
michal-karas <60525701+michal-karas@users.noreply.github.com>
2020-03-20 20:22:42 +0100

    +mediana

--- zadanie_domowe_sobota.py
+++ zadanie_domowe_sobota.py
@@ -31,6 +31,7 @@ with open("c:/Users/User/Desktop/Python/03_Python/zadanie domowe/przykladowy.csv
 # * Znajdź medianę ocen filmów RdN
 # * Znajdź rok, w którym RdN wydał najwięcej filmów
 
+#najgorszy film
 worst_score = 100
 worst_score = data[0]["score"]
 for entry in data:
@@ -41,7 +42,7 @@ for entry in data:
 print(f"Najgorsza ocena filmu RdN to: {worst_score}")
 print(f"Tytuł najgorszego filmu to {worst_title}")
 
-
+#najlepszy film
 best_score = 100
 best_score = data[0]["score"]
 for entry in data:
@@ -52,7 +53,7 @@ for entry in data:
 print(f"Najlepsza ocena filmu RdN to: {best_score}")
 print(f"Tytuł najlepszego filmu to {best_title}")
 
-
+#średnia
 suma = 0
 list_of_elements = 0
 for entry in data:
@@ -60,4 +61,26 @@ for entry in data:
     list_of_elements += 1
 
 print("Średnia ocen z wszytskich filmów Roberta to: ", suma / list_of_elements)
+
+#mediana
+mediana_list=[]
+liczba_elementow = 0
+for entry in data:
+        worst_score = entry["score"]
+        mediana_list.append(worst_score)
+        liczba_elementow += 1
+
+x = sorted(mediana_list)
+print(x)
+print ("Ilość liczb w tabeli to: ", liczba_elementow)
+print ("wyznaczamy środek- tutaj zostanie podana cyfra środka ciągu : ", liczba_elementow / 2)
+q1 = x[43+1]
+q2 = x [44+1]
+print ("43cia cyfra w ciągu to : ", q1)
+print ("44ta cyfra w ciągu to :", q2)
+mediana = (q1 + q2) / 2
+print ("mediana wynosi :", mediana)
+
+
+        
 
